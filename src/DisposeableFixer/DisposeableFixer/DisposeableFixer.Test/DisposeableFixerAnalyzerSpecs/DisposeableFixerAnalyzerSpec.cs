7f4b1b213b65cbc554f3026e7e979e37939af208
David Störmer <noemail@email.de>
2016-11-04 20:54:49 +0100

    Fixes #4

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/DisposeableFixerAnalyzerSpec.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/DisposeableFixerAnalyzerSpec.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.IO;
+using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using NUnit.Framework;
 
@@ -53,8 +54,7 @@ namespace DisFixerTest
     }
 
     [TestFixture]
-    internal class If_Analyser_runs_on_class_with_a_local_initialized_MemoryStream_field_that_is_not_disposed : DisposeableFixerAnalyzerSpec
-    {
+    internal class If_Analyser_runs_on_class_with_a_local_initialized_MemoryStream_field_that_is_not_disposed : DisposeableFixerAnalyzerSpec {
         private Diagnostic[] _diagnostics;
 
         protected override void BecauseOf() {
@@ -102,5 +102,47 @@ namespace DisFixerTest {
         public void Then_there_should_be_one_Diagnostics() {
             _diagnostics.Length.Should().Be(1);
         }
+    }
+    [TestFixture]
+    internal class If_Analyser_runs_on_class_with_a_local_initialized_MemoryStream_in_Ctor_Variable_that_is_not_disposed : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+namespace DisFixerTest {
+    class ClassWithUndisposedVariableInCtor {
+        public ClassWithUndisposedVariableInCtor() {
+            var mem = new MemoryStream();
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_one_Diagnostics() {
+            _diagnostics.Length.Should().Be(1);
+        }
+    }
+}
+
+
+
+
+
+namespace DisFixerTest {
+    class ClassWithUndisposedMemoryStreamAsFieldThatIsInitializedInCtor {
+        private readonly MemoryStream _memStream;
+        public ClassWithUndisposedMemoryStreamAsFieldThatIsInitializedInCtor() {
+            _memStream = new MemoryStream();
+        }
+    }
+}
+namespace DisFixerTest {
+    internal class ClassWithUndisposedMemoryStreamAsField {
+        private readonly MemoryStream _memoryStream = new MemoryStream();
     }
 }
--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer/DiagnosticAnalyzer.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer/DiagnosticAnalyzer.cs
@@ -10,35 +10,49 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace DisposeableFixer {
+namespace DisposeableFixer
+{
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class DisposeableFixerAnalyzer : DiagnosticAnalyzer {
+    public class DisposeableFixerAnalyzer : DiagnosticAnalyzer
+    {
         public const string DiagnosticId = "DisposeableFixer";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof (Resources));
+
+        private static readonly LocalizableString MessageFormat =
+            new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager,
+                typeof (Resources));
+
+        private static readonly LocalizableString Description =
+            new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager,
+                typeof (Resources));
+
         private const string Category = "Wrong Usage";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category,
+            DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
-
-        public override void Initialize(AnalysisContext context) {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSyntaxNodeAction(AnalyseSyntaxNode,SyntaxKind.LocalDeclarationStatement);
-            context.RegisterSyntaxNodeAction(AnalyseSyntaxNode,SyntaxKind.FieldDeclaration);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
         }
 
-        private static void AnalyseSyntaxNode(SyntaxNodeAnalysisContext context)
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSyntaxNodeAction(AnalyseLocalDeclarationStatement, SyntaxKind.LocalDeclarationStatement);
+            context.RegisterSyntaxNodeAction(AnalyseLocalDeclarationStatement, SyntaxKind.FieldDeclaration);
+            context.RegisterSyntaxNodeAction(SimpleAssignmentExpression, SyntaxKind.SimpleAssignmentExpression);
+        }
+        
+        private static void AnalyseLocalDeclarationStatement(SyntaxNodeAnalysisContext context)
         {
             var node = context.Node;
-            var tree = node.SyntaxTree;
             var symanticModel = context.SemanticModel;
-            var test = new StringBuilder();
             var creation = node
                 .DescendantNodes()
                 .OfType<VariableDeclaratorSyntax>()
@@ -51,8 +65,9 @@ namespace DisposeableFixer {
             if (!typeInfo.AllInterfaces.Any(i => i.Name == "IDisposable")) return;
 
             var name = creation.Identifier.Text;
-            var location = creation.DescendantNodes().OfType<ObjectCreationExpressionSyntax>().FirstOrDefault().GetLocation();
-            
+            var location =
+                creation.DescendantNodes().OfType<ObjectCreationExpressionSyntax>().FirstOrDefault().GetLocation();
+
             //find all definitions for a variable wihtin the class and all its methods
             //var usages = context.SemanticModel.SyntaxTree.GetRoot().DescendantNodes().OfType<VariableDeclarationSyntax>();
             var usages = context.SemanticModel.SyntaxTree.GetRoot().DescendantNodes();
@@ -70,6 +85,78 @@ namespace DisposeableFixer {
                 var diagnostic = Diagnostic.Create(Rule, location);
                 context.ReportDiagnostic(diagnostic);
             }
-        }   
+        }
+
+        private static void SimpleAssignmentExpression(SyntaxNodeAnalysisContext context)
+        {
+            var symanticModel = context.SemanticModel;
+            
+            var creationSyntax = context.Node
+                .DescendantNodes()
+                .OfType<ObjectCreationExpressionSyntax>()
+                .FirstOrDefault();
+
+            if (creationSyntax == null) return; //nothing to analyse
+            var identifierNameSyntax = context.Node.DescendantNodes().OfType<IdentifierNameSyntax>().FirstOrDefault();
+            /*at this point we cant get the type of the variable, because its assignment to a field.
+             * We have to find the field and get the name from that FieldDeclaration */
+            var classDeclaration = FindContainingClass(context.Node);
+            if (classDeclaration == null) return;
+            var fieldDeclaration = FindFieldInClass(classDeclaration, identifierNameSyntax.Identifier.Text);
+            if (fieldDeclaration == null) return;
+            var identifierSyntax = fieldDeclaration.DescendantNodes()
+                .OfType<IdentifierNameSyntax>()
+                .FirstOrDefault();
+
+            var typeInfo = symanticModel.GetSymbolInfo(identifierSyntax).Symbol as INamedTypeSymbol;
+            if (typeInfo == null) return;
+            if (!typeInfo.AllInterfaces.Any(i => i.Name == "IDisposable")) return;
+
+            var name = identifierSyntax.Identifier.Text;
+            var location = creationSyntax.GetLocation();
+
+            //find all definitions for a variable wihtin the class and all its methods
+            //var usages = context.SemanticModel.SyntaxTree.GetRoot().DescendantNodes().OfType<VariableDeclarationSyntax>();
+            var usages = context.SemanticModel.SyntaxTree.GetRoot().DescendantNodes();
+            var access = usages.OfType<MemberAccessExpressionSyntax>();
+
+            var dispose = access
+                .Where(a => {
+                    var id = a.Expression as IdentifierNameSyntax;
+                    return id?.Identifier.Text == name && a.Name.Identifier.Text == "Dispose";
+                });
+
+            if (!dispose.Any()) {
+                var diagnostic = Diagnostic.Create(Rule, location);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static FieldDeclarationSyntax FindFieldInClass(ClassDeclarationSyntax classNode, string field_name)
+        {
+            return classNode
+                .DescendantNodes()
+                .OfType<FieldDeclarationSyntax>()
+                .Where(fds =>
+                {
+                    return fds
+                            .DescendantNodes()
+                            .OfType<VariableDeclarationSyntax>()
+                            .Count(id => id.Variables.Any(v => v.Identifier.Text== field_name)) == 1;
+                })
+                .FirstOrDefault();
+        }
+
+        private static ClassDeclarationSyntax FindContainingClass(SyntaxNode node)
+        {
+            while (true)
+            {
+                var @class = node.Parent as ClassDeclarationSyntax;
+                if (@class != null)
+                    return @class;
+
+                node = node.Parent;
+            }
+        }
     }
 }
