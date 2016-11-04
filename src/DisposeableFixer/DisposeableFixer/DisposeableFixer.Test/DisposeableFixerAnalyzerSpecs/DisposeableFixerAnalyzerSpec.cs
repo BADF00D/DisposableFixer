1d7f82c64c92a3e4b036a64eb78985d83ad245ad
David Störmer <noemail@email.de>
2016-11-04 22:46:20 +0100

    Added test to verify problem with issue #3

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/DisposeableFixerAnalyzerSpec.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/DisposeableFixerAnalyzerSpec.cs
@@ -127,22 +127,151 @@ namespace DisFixerTest {
             _diagnostics.Length.Should().Be(1);
         }
     }
+
+    [TestFixture]
+    internal class If_Analyser_runs_on_class_with_that_declares_a_MemoryStream_within_an_using_block    : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+namespace DisFixerTest.UsingBlock
+{
+    public class ClassWithMemoryStreamDeclaredInUsingBlock
+    {
+        public ClassWithMemoryStreamDeclaredInUsingBlock()
+        {
+            using (var mem = new MemoryStream()){}
+        } 
+    }
 }
+";
 
+        [Test]
+        public void Then_there_should_be_one_Diagnostics() {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
 
+    [TestFixture]
+    internal class If_Analyser_runs_on_class_that_uses_a_MemoryStream_within_an_using_block_within_a_Ctor : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
 
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
 
+        private const string Code = @"
+using System.IO;
+namespace DisFixerTest.UsingBlock
+{
+    public class ClassThatUsedMemoryStreamWithinUsingBlock
+    {
+        public ClassThatUsedMemoryStreamWithinUsingBlock()
+        {
+            var memstream = new MemoryStream();
+            using (memstream) { }
+        } 
+    }
+}
+";
 
-namespace DisFixerTest {
-    class ClassWithUndisposedMemoryStreamAsFieldThatIsInitializedInCtor {
-        private readonly MemoryStream _memStream;
-        public ClassWithUndisposedMemoryStreamAsFieldThatIsInitializedInCtor() {
-            _memStream = new MemoryStream();
+        [Test]
+        public void Then_there_should_be_one_Diagnostics() {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+
+    [TestFixture]
+    internal class If_Analyser_runs_on_class_that_uses_a_MemoryStream_within_an_using_block_within_a_method : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+namespace DisFixerTest.UsingBlock {
+    public class ClassThatUsedMemoryStreamWithinUsingBlock {
+        public void SomeMethod() {
+            var memstream = new MemoryStream();
+            using (memstream) { }
         }
     }
 }
-namespace DisFixerTest {
-    internal class ClassWithUndisposedMemoryStreamAsField {
-        private readonly MemoryStream _memoryStream = new MemoryStream();
+";
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics() {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+
+    [TestFixture]
+    internal class If_Analyser_runs_on_class_that_has_a_using_block_in_its_method_that_is_not_used_for_the_MemoryStream : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+namespace DisFixerTest.UsingBlock {
+    public class ClassThatUsedMemoryStreamWithinUsingBlock {
+        public void SomeMethod() {
+            var memstream = new MemoryStream();
+            var memstream2 = new MemoryStream();
+            using (memstream) { }
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_one_Diagnostics() {
+            _diagnostics.Length.Should().Be(1);
+        }
+    }
+
+    [TestFixture]
+    internal class If_Analyser_runs_on_class_that_has_a_using_block_in_its_ctor_that_is_not_used_for_the_MemoryStream : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+namespace DisFixerTest.UsingBlock {
+    public class ClassThatUsedMemoryStreamWithinUsingBlock {
+        public ClassThatUsedMemoryStreamWithinUsingBlock() {
+            var memstream = new MemoryStream();
+            var memstream2 = new MemoryStream();
+            using (memstream) { }
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_one_Diagnostics() {
+            _diagnostics.Length.Should().Be(1);
+        }
+    }
+}
+
+namespace DisFixerTest.UsingBlock {
+    public class ClassThatUsedMemoryStreamWithinUsingBlock {
+        public ClassThatUsedMemoryStreamWithinUsingBlock() {
+            var memstream = new MemoryStream();
+            var memstream2 = new MemoryStream();
+            using (memstream) { }
+        }
     }
 }
