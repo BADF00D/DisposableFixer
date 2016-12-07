92df725316062f09416b9943d3e7ebb8ae888bb9
David Störmer <noemail@email.de>
2016-12-07 16:21:52 +0100

    Added test fro issue 11.

---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/IssueTests/IssueSpec.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests
+{
+    [TestFixture]
+    internal abstract class IssueSpec : Spec
+    {
+        protected readonly DisposableFixerAnalyzer Sut;
+
+        protected IssueSpec()
+        {
+            Sut = new DisposableFixerAnalyzer();
+        }
+    }
+}
