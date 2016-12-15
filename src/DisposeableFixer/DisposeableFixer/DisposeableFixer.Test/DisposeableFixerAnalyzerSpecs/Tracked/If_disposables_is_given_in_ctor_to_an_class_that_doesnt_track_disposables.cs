af916c0923dfcc563ef4e51277103ea6b8af579f
David Störmer <noemail@email.de>
2016-12-15 19:48:25 +0100

    Added some more tests.

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Tracked/If_disposables_is_given_in_ctor_to_an_class_that_doesnt_track_disposables.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Tracked/If_disposables_is_given_in_ctor_to_an_class_that_doesnt_track_disposables.cs
@@ -1,17 +1,42 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using NUnit.Framework;
 
 namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.Tracked
 {
     internal class If_disposables_is_given_in_ctor_to_an_class_that_doesnt_track_disposables : DisposeableFixerAnalyzerSpec {
-        private Diagnostic[] _diagnostics;
+        
 
-        protected override void BecauseOf() {
-            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void Then_there_should_be_no_Diagnostics(string code, int numberOfDisgnostics)
+        {
+            var diagnostics = MyHelper.RunAnalyser(code, Sut);
+            diagnostics.Length.Should().Be(numberOfDisgnostics);
         }
 
-        private const string Code = @"
+        public static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(LocalDeclarationWithObjectCreation, 1)
+                    .SetName("LocalDeclaration with ObjectCreation given to a non tracking instance");
+                yield return new TestCaseData(FactoryCallGivenToNonTrackingInstance, 1)
+                    .SetName("Factory call given to a non tracking instance");
+                yield return new TestCaseData(ObjectCreationGivenToNonTrackingInstance, 1)
+                    .SetName("LocalDeclaration with ObjectCreation given to a non tracking instance");
+                yield return new TestCaseData(FactoryCallWithinCtorCallOfNonTrackingInstance, 1)
+                   .SetName("FactoryCall within call to ctor of a non tracking instance");
+
+            }
+        }
+
+
+
+        private const string LocalDeclarationWithObjectCreation = @"
 using System;
 using System.IO;
 
@@ -32,9 +57,76 @@ namespace DisFixerTest.Tracking {
 }
 ";
 
-        [Test]
-        public void Then_there_should_be_no_Diagnostics() {
-            _diagnostics.Length.Should().Be(1);
+        private const string ObjectCreationGivenToNonTrackingInstance = @"
+using System;
+using System.IO;
+
+namespace DisFixerTest.Tracking {
+    class NoneTracking : IDisposable {
+        public static void Do() {
+            using(var tracking = new NoneTracking(new MemoryStream())) { }
+        }
+
+        public NoneTracking(IDisposable disp) {}
+
+        public void Dispose() {
+            throw new NotImplementedException();
         }
     }
 }
+";
+        private const string FactoryCallGivenToNonTrackingInstance = @"
+namespace DisFixerTest.Tracking {
+    class NoneTracking : IDisposable {
+        public static void Do() {
+            var factory = new MemStreamFactory();
+            var mem = factory.Create();
+
+            using (var tracking = new NoneTracking(mem)) { }
+        }
+
+        public NoneTracking(IDisposable disp) { }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class MemStreamFactory
+    {
+        public MemoryStream Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}
+";
+        private const string FactoryCallWithinCtorCallOfNonTrackingInstance = @"
+namespace DisFixerTest.Tracking {
+    class NoneTracking : IDisposable {
+        public static void Do() {
+            var factory = new MemStreamFactory();
+
+            using (var tracking = new NoneTracking(factory.Create())) { }
+        }
+
+        public NoneTracking(IDisposable disp) { }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class MemStreamFactory
+    {
+        public MemoryStream Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}
+";
+    }
+}
+
+
---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Using/If_disposables_is_using_within_using.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.Using
+{
+    internal class If_disposables_is_using_within_using : DisposeableFixerAnalyzerSpec {
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void Then_there_should_be_no_Diagnostics(string code, int numberOfDisgnostics)
+        {
+            var diagnostics = MyHelper.RunAnalyser(code, Sut);
+            diagnostics.Length.Should().Be(numberOfDisgnostics);
+        }
+
+        public static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(LocalDeclarationWithObjectCreationThatIsCorrectlyDisposed, 0)
+                    .SetName("LocalDeclaration with ObjectCreation given to a non tracking instance that is correctly disposed");
+                yield return new TestCaseData(FactoryCallThatIsCorrectlyDisposed, 0)
+                    .SetName("FactoryCall that is correctly disposed");
+                yield return new TestCaseData(ObjectCreationInUsingThatIsCorrectlyDisposed, 0)
+                    .SetName("ObjectCreation in using that is correctly disposed");
+                yield return new TestCaseData(FactoryCallInUsingThatIsCorrectlyDisposed, 0)
+                    .SetName("FactoryCall in using that is correctly disposed");
+            }
+        }
+
+        private const string LocalDeclarationWithObjectCreationThatIsCorrectlyDisposed = @"
+using System;
+using System.IO;
+
+namespace DisFixerTest.Tracking {
+    class Test : IDisposable {
+        public static void Do() {
+            var mem = new MemoryStream();
+
+            using(mem) { }
+        }
+    }
+}
+";
+
+        private const string ObjectCreationInUsingThatIsCorrectlyDisposed = @"
+using System;
+using System.IO;
+
+namespace DisFixerTest.Tracking {
+    class Test : IDisposable {
+        public static void Do() {
+            using(new MemoryStream()) { }
+        }
+    }
+}
+";
+
+        private const string FactoryCallThatIsCorrectlyDisposed = @"
+namespace DisFixerTest.Tracking {
+    class Test : IDisposable {
+        public static void Do() {
+            var factory = new MemStreamFactory();
+            var mem = factory.Create();
+
+            using (mem) { }
+        }
+    }
+
+    internal class MemStreamFactory
+    {
+        public MemoryStream Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}
+";
+
+        private const string FactoryCallInUsingThatIsCorrectlyDisposed = @"
+namespace DisFixerTest.Tracking {
+    class NoneTracking : IDisposable {
+        public static void Do() {
+            var factory = new MemStreamFactory();
+            using (factory.Create()) { }
+        }
+    }
+
+    internal class MemStreamFactory
+    {
+        public MemoryStream Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}
+";
+    }
+}
+
+
