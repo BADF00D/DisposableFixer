dea4a5c6c331a084f78e09d6de46ca5eddbe19f3
David Störmer <noemail@email.de>
2016-12-15 19:23:16 +0100

    Added tests to verify #16

---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Tracked/If_disposables_is_given_in_ctor_to_an_class_that_doesnt_track_disposables.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.Tracked
+{
+    internal class If_disposables_is_given_in_ctor_to_an_class_that_doesnt_track_disposables : DisposeableFixerAnalyzerSpec {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf() {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System;
+using System.IO;
+
+namespace DisFixerTest.Tracking {
+    class NoneTracking : IDisposable {
+        public static void Do() {
+            var mem = new MemoryStream();
+
+            using(var tracking = new NoneTracking(mem)) { }
+        }
+
+        public NoneTracking(IDisposable disp) {}
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics() {
+            _diagnostics.Length.Should().Be(1);
+        }
+    }
+}
---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Tracked/If_disposables_is_given_in_ctor_to_an_class_that_tracks_disposables.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.Tracked
+{
+    internal class If_disposables_is_given_in_ctor_to_an_class_that_tracks_disposables : DisposeableFixerAnalyzerSpec
+    {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+
+namespace DisFixerTest.Tracking {
+    internal class Tracking {
+        public static void Do() {
+            var mem = new MemoryStream();
+
+            using (var tracking = new StreamReader(mem)) { }
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
