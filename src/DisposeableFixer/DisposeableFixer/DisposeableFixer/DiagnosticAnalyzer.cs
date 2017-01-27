099b0c675e61c004faad3eac60aa1da75df95f2f
David Störmer <David.Stoermer@copra-system.de>
2017-01-27 14:05:44 +0100

    All classes from mscorelib.dll that close a stream on dispose, now gets recognized as tracking class:
    * System.IO.StreamReader
    * System.IO.StreamWriter
    * System.IO.BinaryReader
    * System.IO.BinaryWriter
    * System.IO.BufferedStream
    * System.Security.Cryptography.CryptoStream
    * System.Resources.ResourceReader
    * System.Resources.ResourceSet
    * System.Resources.ResourceWriter

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer/DiagnosticAnalyzer.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer/DiagnosticAnalyzer.cs
@@ -32,7 +32,15 @@ namespace DisposableFixer
 
         private static readonly HashSet<string> TrackingTypes = new HashSet<string>
         {
-            "System.IO.StreamReader"
+            "System.IO.StreamReader",
+            "System.IO.StreamWriter",
+            "System.IO.BinaryReader",
+            "System.IO.BinaryWriter",
+			"System.IO.BufferedStream",
+			"System.Security.Cryptography.CryptoStream",
+			"System.Resources.ResourceReader",
+			"System.Resources.ResourceSet",
+			"System.Resources.ResourceWriter",
         };
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
