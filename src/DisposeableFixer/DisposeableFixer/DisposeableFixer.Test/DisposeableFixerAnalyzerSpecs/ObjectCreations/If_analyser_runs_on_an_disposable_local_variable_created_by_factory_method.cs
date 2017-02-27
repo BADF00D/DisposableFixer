90368404c55c3526ab86b5052d47b560d3017f13
David Störmer <noemail@email.de>
2017-02-27 19:59:58 +0100

    Added test to verify problem of issue #24

---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/ObjectCreations/If_analyser_runs_on_an_disposable_local_variable_created_by_factory_method.cs
@@ -0,0 +1,47 @@
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.ObjectCreations
+{
+    [TestFixture]
+    internal class If_analyser_runs_on_an_disposable_local_variable_created_by_factory_method : DisposeableFixerAnalyzerSpec
+    {
+        private readonly string _code = @"
+using System;
+using System.IO;
+
+using System.IO;
+namespace GivenToNonDisposedTrackingInstance {
+	internal class Program {
+
+            public IDisposable SomeMethod()
+            {
+                var reader = Create();
+                return reader;
+            }
+
+            private static StreamReader Create()
+            {
+                var memoryStream = new MemoryStream();
+                return new StreamReader(memoryStream);
+            }
+        }
+    }
+}";
+
+        private Diagnostic[] _diagnostics;
+
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(_code, Sut);
+        }
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/IssueTests/If_Analyser_runs_on_code_from_issue_24.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests
+{
+    [TestFixture]
+    internal class If_Analyser_runs_on_code_from_issue_24 : IssueSpec
+    {
+        private const string Code = @"
+using System.IO;
+namespace GivenToNonDisposedTrackingInstance {
+	internal class Program {
+
+            public IDisposable SomeMethod()
+            {
+                var reader = Create();//this was marked as not disposed
+                return reader;
+            }
+
+            private static StreamReader Create()
+            {
+                var memoryStream = new MemoryStream();
+                return new StreamReader(memoryStream);
+            }
+        }
+    }
+}";
+
+        private Diagnostic[] _diagnostics;
+
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
