4de2d473089e3df72864122440f1340b02e6625a
David Störmer <noemail@email.de>
2017-07-13 20:59:36 +0200

    Added more tests for null-conditional operator

---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/NullConditionalOperator/If_Analyser_runs_on_a_local_variable_disposed_via_null_conditional_operator.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.NullConditionalOperator
+{
+    [TestFixture]
+    internal class If_Analyser_runs_on_a_local_variable_disposed_via_null_conditional_operator :
+        DisposeableFixerAnalyzerSpec
+    {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        private const string Code = @"
+using System.IO;
+using System.Threading.Tasks;
+namespace DisFixerTest.Async
+{
+    internal class MyClass : IDisposable
+    {
+        public MyClass()
+        {
+            var exampleDisposable = new MemoryStream();
+            exampleDisposable?.Dispose();
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
