5b3a21afcab83ee392af2d17c9acb258b09edc21
David Störmer <badf00d@posteo.de>
2017-08-12 09:57:36 +0200

    Added tests to verify issue #55

---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/DisposeOutSideOfDisposeMethod/If_Analsyser_runs_on_field_dispose_in_method_called_Cleanup - Copy.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.DisposeOutSideOfDisposeMethod
+{
+    [TestFixture]
+    internal class If_Analsyser_runs_on_field_dispose_in_method_decorected_with_attribute_named_TearDown :
+        DisposeableFixerAnalyzerSpec
+    {
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+        private const string Code = @"
+namespace SomeNamespace
+    public class SomeSpec : Spec{
+        private System.IDisposable _memstream;
+
+        public void Create(){
+            _memstream = new System.IO.MemoryStream();
+        }        
+
+        [TearDown]
+        public void TearDown(){
+            _memstream?.Dispose();
+        }
+
+        private class TearDownAttribute : System.Attribute
+        {
+            
+        }
+    }
+}";
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
