f1a01f285bdee0ed70b35ecc5b463a1fc6eede3c
David Störmer <badf00d@posteo.de>
2017-08-23 19:47:14 +0200

    Refactoring.

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Tracked/If_disposables_is_given_in_ctor_to_an_class_that_tracks_disposables.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/Tracked/If_disposables_is_given_in_ctor_to_an_class_that_tracks_disposables.cs
@@ -6,59 +6,29 @@ namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.Tracked
 {
     internal class If_disposables_is_given_in_ctor_to_an_class_that_tracks_disposables : DisposeableFixerAnalyzerSpec
     {
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void Then_there_should_be_no_Diagnostics_for(string code, int numberOfDisgnostics) {
+            var diagnostics = MyHelper.RunAnalyser(code, Sut);
+            diagnostics.Length.Should().Be(numberOfDisgnostics);
+        }
+
         public static IEnumerable<TestCaseData> TestCases
         {
             get
             {
-                yield return new TestCaseData(LocalDeclarationWithObjectCreation, 0)
-                    .SetName("LocalDeclaration with ObjectCreation given to a tracking instance");
-                yield return new TestCaseData(FactoryCallGivenToNonTrackingInstance, 0)
-                    .SetName("Factory call given to a tracking instance");
-                yield return new TestCaseData(ObjectCreationInCallToCtorOfNonTrackingInstance, 0)
-                    .SetName("ObjectCreation in call to ctor of a tracking instance inside of using block");
-                yield return new TestCaseData(FactoryCallWithinCtorCallOfNonTrackingInstance, 0)
-                    .SetName("FactoryCall in call to ctor of a tracking instance");
-                yield return new TestCaseData(ObjectCreationInCallToCtorOfNonTrackingInstanceOutsideAUsingBlock, 0)
-                    .SetName("ObjectCreation in call to ctor of a tracking instance outside of using block");
+                yield return CreateTestCaseForLocalDeclarationWithObjectCreation();
+                yield return CreateTestCaseForFactoryCallGivenToNonTrackingInstance();
+                yield return CreateTestCaseForObjectCreationInCallToCtorOfNonTrackingInstance();
+                yield return CreateTestCaseForFactoryCallWithinCtorCallOfNonTrackingInstance();
+                yield return CreateTestCaseForObjectCreationInCallToCtorOfNonTrackingInstanceOutsideAUsingBlock();
             }
         }
 
+        
 
-        private const string LocalDeclarationWithObjectCreation = @"
-using System;
-using System.IO;
-
-namespace DisFixerTest.Tracking {
-    class Tracking : IDisposable {
-        public static void Do() {
-            var mem = new MemoryStream();
-
-            using (var nontracking = new StreamReader(mem)) { }
-        }
-        public void Dispose() {
-            throw new NotImplementedException();
-        }
-    }
-}
-";
-
-        private const string ObjectCreationInCallToCtorOfNonTrackingInstance = @"
-using System;
-using System.IO;
-
-namespace DisFixerTest.Tracking {
-    class Tracking : IDisposable {
-        public static void Do() {
-            using(var nontracking = new StreamReader(new MemoryStream())) { }
-        }
-        public void Dispose() {
-            throw new NotImplementedException();
-        }
-    }
-}
-";
-
-        private const string ObjectCreationInCallToCtorOfNonTrackingInstanceOutsideAUsingBlock = @"
+        private static TestCaseData CreateTestCaseForObjectCreationInCallToCtorOfNonTrackingInstanceOutsideAUsingBlock()
+        {
+            return new TestCaseData(@"
 using System;
 using System.IO;
 
@@ -73,8 +43,63 @@ namespace DisFixerTest.Tracking {
         }
     }
 }
-";
-        private const string FactoryCallGivenToNonTrackingInstance = @"
+", 0)
+                .SetName("ObjectCreation in call to ctor of a tracking instance outside of using block");
+        }
+
+        private static TestCaseData CreateTestCaseForFactoryCallWithinCtorCallOfNonTrackingInstance()
+        {
+            return new TestCaseData(@"
+using System;
+using System.IO;
+namespace DisFixerTest.Tracking {
+    class Tracking : IDisposable {
+        public static void Do() {
+            var factory = new MemStreamFactory();
+
+            using (var tracking = new StreamReader(factory.Create())) { }
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class MemStreamFactory
+    {
+        public MemoryStream Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}
+", 0)
+                .SetName("FactoryCall in call to ctor of a tracking instance");
+        }
+
+        private static TestCaseData CreateTestCaseForObjectCreationInCallToCtorOfNonTrackingInstance()
+        {
+            return new TestCaseData(@"
+using System;
+using System.IO;
+
+namespace DisFixerTest.Tracking {
+    class Tracking : IDisposable {
+        public static void Do() {
+            using(var nontracking = new StreamReader(new MemoryStream())) { }
+        }
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
+", 0)
+                .SetName("ObjectCreation in call to ctor of a tracking instance inside of using block");
+        }
+
+        private static TestCaseData CreateTestCaseForFactoryCallGivenToNonTrackingInstance()
+        {
+            return new TestCaseData(@"
 using System;
 using System.IO;
 namespace DisFixerTest.Tracking {
@@ -99,39 +124,30 @@ namespace DisFixerTest.Tracking {
         }
     }
 }
-";
-        private const string FactoryCallWithinCtorCallOfNonTrackingInstance = @"
+", 0)
+                .SetName("Factory call given to a tracking instance");
+        }
+
+        private static TestCaseData CreateTestCaseForLocalDeclarationWithObjectCreation()
+        {
+            return new TestCaseData(@"
 using System;
 using System.IO;
+
 namespace DisFixerTest.Tracking {
     class Tracking : IDisposable {
         public static void Do() {
-            var factory = new MemStreamFactory();
+            var mem = new MemoryStream();
 
-            using (var tracking = new StreamReader(factory.Create())) { }
+            using (var nontracking = new StreamReader(mem)) { }
         }
-
         public void Dispose() {
             throw new NotImplementedException();
         }
     }
-
-    internal class MemStreamFactory
-    {
-        public MemoryStream Create()
-        {
-            return new MemoryStream();
-        }
-    }
 }
-";
-
-
-        [Test, TestCaseSource(nameof(TestCases))]
-        public void Then_there_should_be_no_Diagnostics(string code, int numberOfDisgnostics)
-        {
-            var diagnostics = MyHelper.RunAnalyser(code, Sut);
-            diagnostics.Length.Should().Be(numberOfDisgnostics);
+", 0)
+                .SetName("LocalDeclaration with ObjectCreation given to a tracking instance");
         }
     }
 }
