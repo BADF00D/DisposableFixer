f4b5cda131e0417eab1c4748806071f544b504b7
David Störmer <badf00d@posteo.de>
2017-09-05 18:37:51 +0200

    Added test to verify issue #59

---
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/DisposeableFixerAnalyzerSpecs/FuncAndActions/If_analyser_runs_on_an_ObjectCreation_assigned_to_lokal_variable_that_is_returned_later_in_Func.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.FuncAndActions
+{
+    [TestFixture]
+    internal class If_analyser_runs_on_an_ObjectCreation_assigned_to_lokal_variable_that_is_returned_later_in_Func : DisposeableFixerAnalyzerSpec
+    {
+        private const string Code = @"
+using System;
+using System.IO;
+
+namespace DisposeTests {
+    public class TestClass {
+        public TestClass()
+        {
+            Func<IDisposable> func = () =>
+            {
+                var memstream = new MemoryStream();
+
+                return memstream;
+            };
+        }
+    }
+}";
+
+        private Diagnostic[] _diagnostics;
+
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
