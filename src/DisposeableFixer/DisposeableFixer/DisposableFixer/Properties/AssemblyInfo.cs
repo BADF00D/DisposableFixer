cb9cb996390295fcffa176a6b341b56c37c14778
David Störmer <badf00d@posteo.de>
2017-09-23 19:22:00 +0200

    Increment to version 0.32

--- src/DisposeableFixer/DisposeableFixer/DisposableFixer/Properties/AssemblyInfo.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposableFixer/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.31.*")]
-[assembly: AssemblyFileVersion("0.31.0.0")]
+[assembly: AssemblyVersion("0.32.*")]
+[assembly: AssemblyFileVersion("0.32.0.0")]
 [assembly: InternalsVisibleTo("DisposableFixer.Test")]
