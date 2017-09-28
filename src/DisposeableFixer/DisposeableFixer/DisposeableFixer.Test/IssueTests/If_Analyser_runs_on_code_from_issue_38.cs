817a6ed9ba38b041cc205822a107671c272550ad
David Störmer <badf00d@posteo.de>
2017-09-28 19:11:12 +0200

    Added tests to verify #38.

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/IssueTests/If_Analyser_runs_on_code_from_issue_38.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/IssueTests/If_Analyser_runs_on_code_from_issue_38.cs
@@ -1,4 +1,14 @@
-﻿using System;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests
+{
+    [TestFixture]
+    internal class If_Analyser_runs_on_code_from_issue_38 : IssueSpec
+    {
+        private const string Code = @"
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace UsingAspDotNetILoggerFactory
@@ -166,5 +176,20 @@ namespace Microsoft.Extensions.Logging {
     public interface ILoggerFactory : IDisposable {
         ILogger CreateLogger(string categoryName);
         void AddProvider(ILoggerProvider provider);
+    }
+}";
+
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf()
+        {
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostics()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
     }
 }
