84e5aeacb409cd47edd58e0056c58f138e38b455
David Störmer <badf00d@posteo.de>
2017-12-09 13:54:01 +0100

    Cleared Dummy.cs

--- src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/Dummy.cs
+++ src/DisposeableFixer/DisposeableFixer/DisposeableFixer.Test/Dummy.cs
@@ -1,11 +1,1 @@
-﻿using System;
-using System.IO;
-
-namespace StoreObjectAsNonDispsable {
-    internal class AndUseAsIDisposableWithNUllPropagationToDisposeIt {
-        public AndUseAsIDisposableWithNUllPropagationToDisposeIt()
-        {
-            (new MemoryStream() as IDisposable)?.Dispose();
-        }
-    }
-}
+﻿
