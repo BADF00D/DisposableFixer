015a170306ea3f57bbf1fbb5780af554809bf4e6
David Störmer <badf00d@posteo.de>
2018-07-27 09:25:49 +0200

    CodeFix for issue #21 Wrap in using for anonymous ObjectCreations

---
+++ src/DisposableFixer.Test/CodeFix/DisposableAnalyserCodeFixVerifierSpec.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+using TestHelper;
+
+namespace DisposableFixer.Test.CodeFix
+{
+    [DebuggerStepThrough, DebuggerNonUserCode]
+    public class DisposableAnalyserCodeFixVerifierSpec : CodeFixVerifier
+    {
+        private readonly List<Action> _disposeActions = new List<Action>();
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DisposableFixerAnalyzer();
+        }
+
+        public DisposableAnalyserCodeFixVerifierSpec()
+        {
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+        }
+
+        [DebuggerStepThrough]
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            EstablishContext();
+            BecauseOf();
+        }
+
+        [DebuggerStepThrough]
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            foreach (var dispose_action in _disposeActions)
+            {
+                dispose_action();
+            }
+            Cleanup();
+        }
+
+        /// <summary>
+        ///     Test setup. Place your initialization code here.
+        /// </summary>
+        [DebuggerStepThrough]
+        protected virtual void EstablishContext()
+        {
+        }
+
+        /// <summary>
+        ///     Test run. Place the tested method / action here.
+        /// </summary>
+        [DebuggerStepThrough]
+        protected virtual void BecauseOf()
+        {
+        }
+
+        /// <summary>
+        ///     Test clean. Close/delete files, close database connections ..
+        /// </summary>
+        [DebuggerStepThrough]
+        protected virtual void Cleanup()
+        {
+        }
+
+        /// <summary>
+        ///     Creates an Action delegate.
+        /// </summary>
+        /// <param name="func">Method the shall be created as delegate.</param>
+        /// <returns>A delegate of type <see cref="Action" /></returns>
+        protected Action Invoking(Action func)
+        {
+            return func;
+        }
+
+        protected void DisposeOnTearDown(IDisposable disposable)
+        {
+            _disposeActions.Add(() => disposable?.Dispose());
+        }
+
+        protected void PrintCodeToFix(string code)
+        {
+            Console.WriteLine("Code to fix:");
+            Console.WriteLine(code);
+        }
+        protected void PrintFixedCode(string code)
+        {
+            Console.WriteLine("Fixed code:");
+            Console.WriteLine(code);
+        }
+    }
+}
---
+++ src/DisposableFixer/CodeFix/WrapInUsingCodeFixProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DisposableFixer.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace DisposableFixer.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UndisposedMemberCodeFixProvider))]
+    [Shared]
+    public class WrapInUsingCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(
+                SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation
+            );
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var id = context.Diagnostics.First().Id;
+            if (id == SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation)
+                context.RegisterCodeFix(
+                    CodeAction.Create("Wrap in using", c => WrapInUsing(context, c)),
+                    context.Diagnostics);
+
+            return Task.FromResult(1);
+        }
+
+        private async Task<Document> WrapInUsing(CodeFixContext context, CancellationToken cancel)
+        {
+            var oldRoot = await context.Document.GetSyntaxRootAsync(cancel);
+            var node = oldRoot.FindNode(context.Span) as ObjectCreationExpressionSyntax;
+
+            var editor = await DocumentEditor.CreateAsync(context.Document, context.CancellationToken);
+            var @using = SyntaxFactory.UsingStatement(SyntaxFactory.Block())
+                .WithExpression(node);
+            editor.ReplaceNode(node.Parent, @using);
+
+            return editor.GetChangedDocument();
+        }
+    }
+}
