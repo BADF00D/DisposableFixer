dd4724343ba9b2b06787c1fb9b877b21ca19e1a1
David Störmer <badf00d@posteo.de>
2018-07-27 09:54:01 +0200

    #21 Added test for local variable that is not disposed and should be wrapped in using

---
+++ src/DisposableFixer.Test/CodeFix/WrapLocalVariableInUsingCodeFixProviderSpecs/WrapLocalVariableInUsingCodeFixProviderSpec.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using DisposableFixer.CodeFix;
+using DisposableFixer.Extensions;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.CodeFix.WrapLocalVariableInUsingCodeFixProviderSpecs
+{
+    [TestFixture]
+    internal class WrapLocalVariableInUsingCodeFixProviderSpec : DisposableAnalyserCodeFixVerifierSpec
+    {
+        private const string CodeWithUndisposedLocalVariableAndModeCode = @"
+using System;
+using System.IO;
+
+namespace SomeNamespace
+{
+    public class SomeClass
+    {
+        public SomeClass()
+        {
+            var variable = Create();
+            Console.WriteLine();
+        }
+
+        private static IDisposable Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}
+";
+
+        private static IEnumerable<TestCaseData> TestCases {
+            get {
+                yield return new TestCaseData(CodeWithUndisposedLocalVariableAndModeCode,
+                        SyntaxNodeAnalysisContextExtension.IdForNotDisposedLocalVariable)
+                    .SetName("Local varibale and more code");
+            }
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new WrapLocalVariableInUsingCodeFixProvider();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void Should_the_applied_CodeFix_solve_the_diagnostic(string code, string preFixDiagnisticId)
+        {
+            PrintCodeToFix(code);
+            MyHelper.RunAnalyser(code, GetCSharpDiagnosticAnalyzer())
+                .Should().Contain(d => d.Id == preFixDiagnisticId, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(code);
+            PrintFixedCode(fixedCode);
+
+            MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer())
+                .Should().NotContain(d => d.Id == preFixDiagnisticId, "this should have been fixed");
+        }
+    }
+}
---
+++ src/DisposableFixer/CodeFix/WrapLocalVariableInUsingCodeFixProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using DisposableFixer.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace DisposableFixer.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UndisposedMemberCodeFixProvider))]
+    [Shared]
+    public class WrapLocalVariableInUsingCodeFixProvider : CodeFixProvider
+    {
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create<string>(
+            SyntaxNodeAnalysisContextExtension.IdForNotDisposedLocalVariable);
+    }
+}
