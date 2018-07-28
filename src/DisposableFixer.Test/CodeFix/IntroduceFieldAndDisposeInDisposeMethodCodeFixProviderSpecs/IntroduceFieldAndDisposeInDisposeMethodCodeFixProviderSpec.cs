f90c16437d6df4f3984bb492e2f191f2a4294de3
David Störmer <badf00d@posteo.de>
2018-07-28 20:57:48 +0200

    #75 Added test to verify issue.

---
+++ src/DisposableFixer.Test/CodeFix/IntroduceFieldAndDisposeInDisposeMethodCodeFixProviderSpecs/IntroduceFieldAndDisposeInDisposeMethodCodeFixProviderSpec.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using DisposableFixer.CodeFix;
+using DisposableFixer.Extensions;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.CodeFix.IntroduceFieldAndDisposeInDisposeMethodCodeFixProviderSpecs
+{
+    [TestFixture]
+    internal class IntroduceFieldAndDisposeInDisposeMethodCodeFixProviderSpec : DisposableAnalyserCodeFixVerifierSpec
+    {
+        private CodeFixProvider _sut;
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new IntroduceFieldAndDisposeInDisposeMethodCodeFixProvider();
+        }
+
+        protected override void BecauseOf()
+        {
+            _sut = GetCSharpCodeFixProvider();
+        }
+
+        [Test]
+        public void Should_the_CodeFixProvider_be_responsible_for_undispose_local_fields()
+        {
+            _sut.FixableDiagnosticIds.Should()
+                .Contain(SyntaxNodeAnalysisContextExtension.IdForNotDisposedLocalVariable);
+        }
+
+        [Test]
+        public void Should_the_CodeFixProvider_be_responsible_for_anonymous_objects_from_ObjectCreations()
+        {
+            _sut.FixableDiagnosticIds.Should()
+                .Contain(SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation);
+        }
+
+        [Test]
+        public void Should_the_CodeFixProvider_be_responsible_for_anonymous_objects_from_MethodInvocations()
+        {
+            _sut.FixableDiagnosticIds.Should()
+                .Contain(SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromMethodInvocation);
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void Should_CodeFix_work_correkt(string code, string preFixDiagnosticId)
+        {
+            PrintCodeToFix(code);
+            MyHelper.RunAnalyser(code, GetCSharpDiagnosticAnalyzer())
+                .Should().Contain(d => d.Id == preFixDiagnosticId, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(code);
+            PrintFixedCode(fixedCode);
+
+            MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer())
+                .Should().BeEmpty();
+        }
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(AnonymousObjectCreation, SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation)
+                    .SetName("Undisposed anonymous ObjectCreation");
+                yield return new TestCaseData(AnonymousMethodInvoation, SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromMethodInvocation)
+                    .SetName("Undisposed anonymous MethodInvocation");
+                yield return new TestCaseData(LocalVariable, SyntaxNodeAnalysisContextExtension.IdForNotDisposedLocalVariable)
+                    .SetName("Undisposed local variable");
+            }
+        }
+
+        private const string AnonymousObjectCreation = @"
+using System.IO;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            new MemoryStream();
+        }
+    }
+
+}
+";
+        private const string AnonymousMethodInvoation = @"
+using System;
+using System.IO;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            Create();
+        }
+
+        private IDisposable Create()
+        {
+            return new MemoryStream();
+        }
+    }
+
+}
+";
+
+        private const string LocalVariable = @"
+using System;
+using System.IO;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            var localVariable = Create();
+        }
+
+        private IDisposable Create()
+        {
+            return new MemoryStream();
+        }
+    }
+
+}
+";
+    }
+}
---
+++ src/DisposableFixer/CodeFix/IntroduceFieldAndDisposeInDisposeMethodCodeFixProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DisposableFixer.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace DisposableFixer.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UndisposedMemberCodeFixProvider)), Shared]
+    public class IntroduceFieldAndDisposeInDisposeMethodCodeFixProvider : CodeFixProvider
+    {
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnotic = context.Diagnostics.FirstOrDefault();
+            if(diagnotic == null) return Task.CompletedTask;
+
+            if (diagnotic.Id == SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation ||
+                diagnotic.Id == SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromMethodInvocation)
+            {
+                context.RegisterCodeFix(
+                    CodeAction.Create("Create field and dispose in Dispose() method.", cancel => IntroduceFieldAndDisposeInDisposeMethod(context, cancel)),
+                    diagnotic
+                );
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static Task<Document> IntroduceFieldAndDisposeInDisposeMethod(CodeFixContext context, CancellationToken cancel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(
+            SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromMethodInvocation,
+            SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation,
+            SyntaxNodeAnalysisContextExtension.IdForNotDisposedLocalVariable
+        );
+    }
+}
