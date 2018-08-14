d03523eac70c7929da0d2a8d3ca8d6443354328b
David Störmer <badf00d@posteo.de>
2018-08-14 19:48:06 +0200

    #83 Added test to verify issue.

---
+++ src/DisposableFixer.Test/IssueTests/If_Analyser_runs_on_code_from_issue_83.cs
@@ -0,0 +1,55 @@
+using System;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests
+{
+    [TestFixture]
+    internal class If_Analyser_runs_on_code_from_issue_83 : IssueSpec
+    {
+        private const string Code = @"
+using System.Data.SqlClient;
+
+namespace Demo
+{
+    internal class Program
+    {
+        public async void Test()
+        {
+            var reader = new SqlDataReader();
+            reader.Close();
+        }
+    }
+}
+
+namespace System.Data.SqlClient
+{
+    internal class SqlDataReader : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Close()
+        {
+        }
+    }
+}";
+
+        private Diagnostic[] _diagnostics;
+
+        protected override void BecauseOf()
+        {
+            Console.WriteLine(Code);
+
+            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+        }
+
+        [Test]
+        public void Then_there_should_be_no_Diagnostic()
+        {
+            _diagnostics.Length.Should().Be(0);
+        }
+    }
+}
