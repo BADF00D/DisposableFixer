a52162faa7b07ec0bc428b7ff9e8404a7869d633
David Störmer <badf00d@posteo.de>
2018-08-16 19:11:51 +0200

    Increment to version 1.0.1

--- src/DisposableFixer/Properties/AssemblyInfo.cs
+++ src/DisposableFixer/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.40.0.0")]
-[assembly: AssemblyFileVersion("0.40.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: InternalsVisibleTo("DisposableFixer.Test")]
