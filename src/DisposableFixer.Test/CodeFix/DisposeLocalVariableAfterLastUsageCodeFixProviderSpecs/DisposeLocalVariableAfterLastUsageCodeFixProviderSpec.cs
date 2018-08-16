c1140d18976f1fc7ffac2c6bc206e9d381e9095d
David Störmer <badf00d@posteo.de>
2018-08-16 19:58:08 +0200

    #79 Added test to verify not yet existing CodeFix

---
+++ src/DisposableFixer.Test/CodeFix/DisposeLocalVariableAfterLastUsageCodeFixProviderSpecs/DisposeLocalVariableAfterLastUsageCodeFixProviderSpec.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using DisposableFixer.Extensions;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.CodeFix.DisposeLocalVariableAfterLastUsageCodeFixProviderSpecs
+{
+    [TestFixture]
+    internal class DisposeLocalVariableAfterLastUsageCodeFixProvider : DisposableAnalyserCodeFixVerifierSpec
+    {
+        private CodeFixProvider _sut;
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new DisposableFixer.CodeFix.DisposeLocalVariableAfterLastUsageCodeFixProvider();
+        }
+
+        protected override void BecauseOf()
+        {
+            _sut = GetCSharpCodeFixProvider();
+        }
+
+        [Test]
+        public void Should_the_CodeFixProvider_be_responsible_for_undispose_local_fields()
+        {
+            _sut.FixableDiagnosticIds.Should()
+                .Contain(SyntaxNodeAnalysisContextExtension.IdForNotDisposedLocalVariable);
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void Should_CodeFix_work_correkt(string code, string preFixDiagnosticId)
+        {
+            PrintCodeToFix(code);
+            MyHelper.RunAnalyser(code, GetCSharpDiagnosticAnalyzer())
+                .Should().Contain(d => d.Id == preFixDiagnosticId, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(code);
+            PrintFixedCode(fixedCode);
+
+            MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer())
+                .Should().BeEmpty();
+        }
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(AnonymousObjectCreation, SyntaxNodeAnalysisContextExtension.IdForAnonymousObjectFromObjectCreation)
+                    .SetName("Undisposed local Variable");
+            }
+        }
+
+        private const string AnonymousObjectCreation = @"
+using System.IO;
+
+namespace SomeNamespace {
+    internal class SomeClass {
+        public void SomeMethod() {
+            var memoryStream = new MemoryStream();
+            var x = 0;
+            var y = 1;
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var z = 2;
+        }
+    }
+}
+";
+    }
+}
---
+++ src/DisposableFixer/CodeFix/DisposeLocalVariableAfterLastUsageCodeFixProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace DisposableFixer.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UndisposedMemberCodeFixProvider))]
+    [Shared]
+    public class DisposeLocalVariableAfterLastUsageCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; }
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
