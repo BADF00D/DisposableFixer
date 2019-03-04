38d83bca412b71825985247d6da43a6bdf17c74e
David Störmer <badf00d@posteo.de>
2019-03-04 20:53:24 +0100

    Issue #106 Added some more tests.

---
+++ src/DisposableFixer.Test/DisposeableFixerAnalyzerSpecs/InterlockedExcahnge/If_Analyzer_run_on_Memberassigned_via_InterlockedExchange.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.InterlockedExcahnge
+{
+    [TestFixture]
+    internal class If_Analyzer_run_on_Memberassigned_via_InterlockedExchange : DisposeableFixerAnalyzerSpec
+    {
+        private const string AssignedViaLocalVariableCreatedByObjectCreation = @"
+using System;
+using System.IO;
+using System.Threading;
+
+namespace SomeNamespace
+{
+    internal class SomeClass : IDisposable
+    {
+        private readonly IDisposable _field;
+
+        public SomeClass()
+        {
+            var mem = new MemoryStream();
+
+            Interlocked.Exchange(ref _field, mem)
+                ?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _field?.Dispose();
+        }
+    }
+}";
+
+        private const string AssignedByObjectCreation = @"
+using System;
+using System.IO;
+using System.Threading;
+
+namespace SomeNamespace
+{
+    internal class SomeClass : IDisposable
+    {
+        private readonly IDisposable _field;
+
+        public SomeClass()
+        {
+            Interlocked.Exchange(ref _field, new MemoryStream())
+                ?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _field?.Dispose();
+        }
+    }
+}";
+
+        private const string AssignedViaLocalVariableCreatedByMethodInvocation = @"
+using System;
+using System.IO;
+using System.Threading;
+
+namespace SomeNamespace
+{
+    internal class SomeClass : IDisposable
+    {
+        private readonly IDisposable _field;
+
+        public SomeClass()
+        {
+            var mem = Create();
+
+            Interlocked.Exchange(ref _field, mem)
+                ?.Dispose();
+        }
+
+        private IDisposable Create() => new MemoryStream();
+
+        public void Dispose()
+        {
+            _field?.Dispose();
+        }
+    }
+}";
+
+        private const string AssignedByMethodInvocation = @"
+using System;
+using System.IO;
+using System.Threading;
+
+namespace SomeNamespace
+{
+    internal class SomeClass : IDisposable
+    {
+        private readonly IDisposable _field;
+
+        public SomeClass()
+        {
+            Interlocked.Exchange(ref _field, Create())
+                ?.Dispose();
+        }
+
+        private IDisposable Create() => new MemoryStream();
+
+        public void Dispose()
+        {
+            _field?.Dispose();
+        }
+    }
+}";
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(AssignedByMethodInvocation)
+                    .SetName("Assigned by MethodInvocation");
+                yield return new TestCaseData(AssignedByObjectCreation)
+                    .SetName("Assigned by ObjectCreation");
+                yield return new TestCaseData(AssignedViaLocalVariableCreatedByMethodInvocation)
+                    .SetName("Assigned by local variable assigned by MethodCreation");
+                yield return new TestCaseData(AssignedViaLocalVariableCreatedByObjectCreation)
+                    .SetName("Assigned by local variable assigned by ObjectCreation");
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void Then_there_should_be_no_Diagnostics(string code)
+        {
+            var diagnostics = MyHelper.RunAnalyser(AssignedViaLocalVariableCreatedByObjectCreation, Sut);
+            diagnostics.Should().BeEmpty();
+        }
+    }
+}
