8f993529473faadc92720aa3d558bac58261913c
David Störmer <badf00d@posteo.de>
2019-09-13 19:24:19 +0200

    Issue #122 added tests to verify issue.

--- src/DisposableFixer.Test/DisposeableFixerAnalyzerSpecs/Async/If_Analyser_runs_on_class_that_uses_await_in_return_statement.cs
+++ src/DisposableFixer.Test/DisposeableFixerAnalyzerSpecs/Async/If_Analyser_runs_on_class_that_uses_await_in_return_statement.cs
@@ -1,42 +1,152 @@
-using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using NUnit.Framework;
 
 namespace DisposableFixer.Test.DisposeableFixerAnalyzerSpecs.Async
 {
     [TestFixture]
-    internal class If_Analyser_runs_on_class_that_uses_await_in_return_statement :
-        DisposeableFixerAnalyzerSpec
+    internal class If_Analyser_runs_on_a_async_MethodInvocation_assigned_to_member
     {
         private Diagnostic[] _diagnostics;
 
-        protected override void BecauseOf()
+        [TestCaseSource(nameof(TestCases))]
+        public string[] Should_be_correct(string code)
         {
-            _diagnostics = MyHelper.RunAnalyser(Code, Sut);
+            return MyHelper.RunAnalyser(code, new DisposableFixerAnalyzer())
+                .Select(d => d.Descriptor.Id)
+                .ToArray();
         }
 
-        private const string Code = @"
+
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return AssignedToField();
+                yield return AssignedToStaticField();
+                yield return AssignedToProperty();
+                yield return AssignedToStaticProperty();
+            }
+        }
+
+        private static TestCaseData AssignedToField()
+        {
+            const string Code = @"
 using System.IO;
 using System.Threading.Tasks;
-namespace DisFixerTest.Async
+
+namespace SomeNamespace
 {
-    class AsyncFileStream
+    public class SomeClass
     {
-        public Task<MemoryStream> Data()
+        private MemoryStream _mem;
+
+        public async Task Do()
+        {
+            _mem = await Create();
+        }
+        public Task<MemoryStream> Create()
         {
             return Task.FromResult(new MemoryStream());
-        }
-        public async Task<MemoryStream> Test(){
-            return await Data();
         }
     }
 }
 ";
 
-        [Test]
-        public void Then_there_should_be_no_Diagnostics()
-        {
-            _diagnostics.Length.Should().Be(0);
+            return new TestCaseData(Code)
+                .Returns(new[] { NotDisposed.Assignment.FromMethodInvocation.ToFieldNotDisposedDescriptor.Id })
+                .SetName(nameof(AssignedToField));
         }
+
+        private static TestCaseData AssignedToStaticField()
+        {
+            const string Code = @"
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SomeNamespace
+{
+    public class SomeClass
+    {
+        private static MemoryStream _mem;
+
+        public async Task Do()
+        {
+            _mem = await Create();
+        }
+        public Task<MemoryStream> Create()
+        {
+            return Task.FromResult(new MemoryStream());
+        }
+    }
+}
+";
+
+            return new TestCaseData(Code)
+                .Returns(new[] { NotDisposed.Assignment.FromMethodInvocation.ToStaticFieldNotDisposedDescriptor.Id })
+                .SetName(nameof(AssignedToStaticField));
+        }
+
+        private static TestCaseData AssignedToProperty()
+        {
+            const string Code = @"
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SomeNamespace
+{
+    public class SomeClass
+    {
+        private MemoryStream _mem {get; set;}
+
+        public async Task Do()
+        {
+            _mem = await Create();
+        }
+        public Task<MemoryStream> Create()
+        {
+            return Task.FromResult(new MemoryStream());
+        }
+    }
+}
+";
+
+            return new TestCaseData(Code)
+                .Returns(new[] { NotDisposed.Assignment.FromMethodInvocation.ToPropertyNotDisposedDescriptor.Id })
+                .SetName(nameof(AssignedToProperty));
+        }
+
+        private static TestCaseData AssignedToStaticProperty()
+        {
+            const string Code = @"
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SomeNamespace
+{
+    public class SomeClass
+    {
+        private static MemoryStream _mem {get; set;}
+
+        public async Task Do()
+        {
+            _mem = await Create();
+        }
+        public Task<MemoryStream> Create()
+        {
+            return Task.FromResult(new MemoryStream());
+        }
+    }
+}
+";
+
+            return new TestCaseData(Code)
+                .Returns(new[] { NotDisposed.Assignment.FromMethodInvocation.ToStaticPropertyNotDisposedDescriptor.Id })
+                .SetName(nameof(AssignedToStaticProperty));
+        }
+
+
     }
 }
---
+++ src/DisposableFixer.Test/IssueTests/Issues100/Issue122_Wrong_diagnostic.cs
@@ -0,0 +1,39 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests.Issues100
+{
+    [TestFixture]
+    internal class Issue122_Wrong_diagnosticcs : IssueSpec
+    {
+
+        private const string Code = @"
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SomeNamespace
+{
+    public class SomeClass
+    {
+        private MemoryStream _mem;
+
+        public async Task Do()
+        {
+            _mem = await Create();
+        }
+        public Task<MemoryStream> Create()
+        {
+            return Task.FromResult(new MemoryStream());
+        }
+    }
+}";
+        [Test]
+        public void Then_there_should_be_one_Diagnostics_with_Severity_Info()
+        {
+            PrintCodeToAnalyze(Code);
+            var diagnostics = MyHelper.RunAnalyser(Code, new DisposableFixerAnalyzer());
+            diagnostics.Should().HaveCount(1);
+            diagnostics[0].Should().Be(NotDisposed.Assignment.FromMethodInvocation.ToFieldNotDisposedDescriptor);
+        }
+    }
+}
