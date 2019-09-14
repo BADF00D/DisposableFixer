72f0638e2d81a43b4d177f62f592b2e92e9d3c9e
David Störmer <badf00d@posteo.de>
2019-09-14 13:39:17 +0200

    Added test to verify issue #121

---
+++ src/DisposableFixer.Test/IssueTests/Issues100/Issue121_DisposeAfterLastUsage.cs
@@ -0,0 +1,56 @@
+using DisposableFixer.Test.CodeFix;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests.Issues0
+{
+    [TestFixture]
+    internal class Issue121_DisposeAfterLastUsage : DisposableAnalyserCodeFixVerifierSpec
+    {
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new DisposableFixer.CodeFix.DisposeLocalVariableAfterLastUsageCodeFixProvider();
+        }
+
+        private const string Code = @"
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class DisposeAfterLastUsageDoesNotWork
+    {
+        public async Task Do()
+        {
+            var fac = new MemoryStream();
+            var bytesRead = await fac.ReadAsync(new byte[10], 0, 10);
+        }
+    }
+}";
+        [Test]
+        public void Apply_CodeFix_should_not_throw_Exception()
+        {
+            PrintCodeToFix(Code);
+            var beforeCodefixDiagnostics = MyHelper.RunAnalyser(Code, GetCSharpDiagnosticAnalyzer());
+            beforeCodefixDiagnostics
+                .Should().Contain(d => d.Id == Id.ForNotDisposedLocalVariable, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(Code);
+            PrintFixedCode(fixedCode);
+
+            var cSharpCompilerDiagnostics = GetCSharpCompilerErrors(fixedCode);
+            PrintFixedCodeDiagnostics(cSharpCompilerDiagnostics);
+            cSharpCompilerDiagnostics
+                .Should().HaveCount(0, "we don't want to introduce bugs");
+
+            var diagnostics = MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer());
+            diagnostics.Should().HaveCount(0);
+
+            var dispsoePosition = fixedCode.IndexOf("fac.Dispose");
+            var lastUsagePosition = fixedCode.IndexOf("fac.");
+            dispsoePosition.Should().BeGreaterThan(lastUsagePosition);
+        }
+    }
+}
