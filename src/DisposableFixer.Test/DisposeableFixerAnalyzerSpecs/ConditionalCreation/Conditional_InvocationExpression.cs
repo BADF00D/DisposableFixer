aa562b4f67249084aab017641cf0dd3d6913c7c8
David Störmer <badf00d@posteo.de>
2019-10-05 19:27:38 +0200

    Added similar tests to verify issue #131

---
+++ src/DisposableFixer.Test/DisposeableFixerAnalyzerSpecs/ConditionalCreation/Conditional_InvocationExpression.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests.Issues100
+{
+    [TestFixture]
+    internal class Conditional_InvocationExpression : IssueSpec
+    {
+
+        private const string Code = @"
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static Stream _pipe;
+        static void X(string[] args)
+        {
+            StreamWriter Create(Stream s) => new StreamWriter(s);
+            StreamWriter sw = _pipe != null ? Create(_pipe): null;
+            try
+            {
+            }
+            finally
+            {
+                sw?.Dispose();
+            }
+        }
+    }
+}
+
+";
+
+        [Test]
+        public void Then_there_should_be_no_diagnostic()
+        {
+            PrintCodeToAnalyze(Code);
+            var diagnostics = MyHelper.RunAnalyser(Code, new DisposableFixerAnalyzer());
+            diagnostics.Should().HaveCount(0);
+        }
+    }
+}
---
+++ src/DisposableFixer.Test/DisposeableFixerAnalyzerSpecs/ConditionalCreation/Conditional_ObjectCreation.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests.Issues100
+{
+    [TestFixture]
+    internal class Conditional_ObjectCreation : IssueSpec
+    {
+
+        private const string Code = @"
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static Stream _pipe;
+        static void X(string[] args)
+        {
+            StreamWriter sw = _pipe != null ? new StreamWriter(_pipe) { AutoFlush = true } : null;
+            try
+            {
+            }
+            finally
+            {
+                sw?.Dispose();
+            }
+        }
+    }
+}
+";
+
+        [Test]
+        public void Then_there_should_be_no_diagnostic()
+        {
+            PrintCodeToAnalyze(Code);
+            var diagnostics = MyHelper.RunAnalyser(Code, new DisposableFixerAnalyzer());
+            diagnostics.Should().HaveCount(0);
+        }
+    }
+}
