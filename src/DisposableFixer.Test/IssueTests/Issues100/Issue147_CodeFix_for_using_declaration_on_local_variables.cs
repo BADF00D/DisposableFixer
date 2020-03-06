97efeb53e7611419a0cac2f6f4d61caddd465aba
David Störmer <badf00d@posteo.de>
2020-03-06 19:52:42 +0100

    Issue #147 added tests for anonymous objects

---
+++ src/DisposableFixer.Test/IssueTests/Issues100/Issue147_CodeFix_for_using_declaration_on_local_variables.cs
@@ -0,0 +1,125 @@
+using DisposableFixer.CodeFix;
+using DisposableFixer.Test.CodeFix;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using NUnit.Framework;
+
+namespace DisposableFixer.Test.IssueTests.Issues100
+{
+    [TestFixture]
+    internal class Issue147_CodeFix_for_using_declaration_on_local_variables : DisposableAnalyserCodeFixVerifierSpec
+    {
+
+        private const string Code = @"
+using System;
+using System.IO;
+namespace SomeNamespace
+{
+    internal class TestClass
+    {
+        public void SomeMethod()
+        {
+            var localvariable = new MemoryStream();
+        }
+    }
+}
+";
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new WrapLocalVariableInUsingDeclarationCodeFix();
+        }
+
+        [Test]
+        public void Then_there_should_be_no_diagnostic()
+        {
+            PrintCodeToFix(Code);
+            var diagnostics = MyHelper.RunAnalyser(Code, GetCSharpDiagnosticAnalyzer());
+            diagnostics.Should().HaveCount(1);
+            diagnostics[0].Id.Should().Be(Id.ForNotDisposedLocalVariable, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(Code);
+            PrintFixedCode(fixedCode);
+
+            MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer())
+                .Should().BeEmpty();
+        }
+    }
+
+    [TestFixture]
+    internal class Issue147_CodeFix_for_using_declaration_on_anonymous_objects_of_ObjectCreation : DisposableAnalyserCodeFixVerifierSpec
+    {
+
+        private const string Code = @"
+using System.IO;
+namespace SomeNamespace
+{
+    internal class TestClass
+    {
+        public void SomeMethod()
+        {
+            new MemoryStream();
+        }
+    }
+}
+";
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new WrapLocalVariableInUsingDeclarationCodeFix();
+        }
+
+        [Test]
+        public void Then_there_should_be_no_diagnostic()
+        {
+            PrintCodeToFix(Code);
+            var diagnostics = MyHelper.RunAnalyser(Code, GetCSharpDiagnosticAnalyzer());
+            diagnostics.Should().HaveCount(1);
+            diagnostics[0].Id.Should().Be(Id.ForAnonymousObjectFromObjectCreation, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(Code);
+            PrintFixedCode(fixedCode);
+
+            MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer())
+                .Should().BeEmpty();
+        }
+    }
+
+    [TestFixture]
+    internal class Issue147_CodeFix_for_using_declaration_on_anonymous_objects_of_MethodInvocation : DisposableAnalyserCodeFixVerifierSpec
+    {
+
+        private const string Code = @"
+using System;
+using System.IO;
+namespace SomeNamespace
+{
+    internal class TestClass
+    {
+        public void SomeMethod()
+        {
+            IDisposable Create () =>new MemoryStream();
+            Create();
+        }
+    }
+}
+";
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new WrapLocalVariableInUsingDeclarationCodeFix();
+        }
+
+        [Test]
+        public void Then_there_should_be_no_diagnostic()
+        {
+            PrintCodeToFix(Code);
+            var diagnostics = MyHelper.RunAnalyser(Code, GetCSharpDiagnosticAnalyzer());
+            diagnostics.Should().HaveCount(1);
+            diagnostics[0].Id.Should().Be(Id.ForAnonymousObjectFromMethodInvocation, "this should be fixed");
+
+            var fixedCode = ApplyCSharpCodeFix(Code);
+            PrintFixedCode(fixedCode);
+
+            MyHelper.RunAnalyser(fixedCode, GetCSharpDiagnosticAnalyzer())
+                .Should().BeEmpty();
+        }
+    }
+}
---
+++ src/DisposableFixer/CodeFix/IntroductLocalVariableAndUseusingDeclaration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using DisposableFixer.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace DisposableFixer.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UndisposedMemberCodeFixProvider))]
+    [Shared]
+    public class IntroduceLocalVariableAndUseUsingDeclaration : CodeFixProvider
+    {
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static async Task<Document> PrefixWithUsingDeclaration(CodeFixContext context, CancellationToken cancel)
+        {
+            return await Task.FromResult(context.Document);
+        }
+
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(Id.ForAnonymousObjectFromObjectCreation, Id.ForAnonymousObjectFromMethodInvocation);
+        public override FixAllProvider GetFixAllProvider() => null;
+    }
+}
